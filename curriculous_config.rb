4dcea2c521042d1fea0171c91751e987298073bb
Pito Salas <pitosalas@gmail.com>
2013-09-10 19:58:01 -0400

    first commit

---
+++ curriculous_config.rb
@@ -0,0 +1,11 @@
+# Configure Curriculous
+
+# URL for AWS Deployment of the course
+C_AWS_BUCKET = "cosi235a.courses.salas.com"
+
+# Local directory path for course content directory
+C_CONTENT_DIR = "/mydev/curriculous-content-cosi235a"
+
+# Local directory path for directory of topics
+C_TOPICS_DIR = "/mydev/curriculous-topics"
+
