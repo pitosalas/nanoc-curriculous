5c4c8989e3f8206d1d25681364b1d2d9799f4e43
Pito Salas <pitosalas@gmail.com>
2013-10-07 16:52:23 -0400

    Add rake content:grab commands and related support methods in rake_helper. Lots of experimenting to learn Rake subtleties at teh same time. I am still not happy with the architecture of this thing.

--- rake/rake_helper.rb
+++ rake/rake_helper.rb
@@ -8,53 +8,77 @@ CONF_FILE_PATH_TEMPLATE = "rake/config_curr.erb"
 RUN_CONF_PATH = "lib/runtime_config.rb"
 RUN_CONF_PATH_TEMPLATE = "rake/runtime_conf.erb"
 
+module RakeHelpers
 Rake::TaskManager.record_task_metadata = true
+#Rake.application.options.trace_rules = true
 
-def verify_environment
-	if File.exists?(CONF_FILE_PATH)
-		require_relative "../"+CONF_FILE_PATH
-	else
-		abort "ERROR: Didn't find #{CONF_FILE_PATH}. Use rake conf:declare task to create it."
-	end
-end
-
-def write_config_file content_path
-	puts "[Writing #{CONF_FILE_PATH} file for #{content_path}]"
-	File.open(CONF_FILE_PATH_TEMPLATE) do |f|
-		erb = ERB.new(f.read)
-		File.open(CONF_FILE_PATH, 'w') do |f|
-			f.write erb.result(binding)
+	def verify_environment
+		if File.exists?(CONF_FILE_PATH)
+			require_relative "../"+CONF_FILE_PATH
+		else
+			fail "ERROR: Didn't find #{CONF_FILE_PATH}. Use rake conf:declare task to create it."
 		end
 	end
-end
 
-def write_runtime_config
-	puts "[Writing #{RUN_CONF_PATH}/curriculous_config.rb]"
-	File.open(RUN_CONF_PATH_TEMPLATE) do |f|
-		erb = ERB.new(f.read)
-		File.open(RUN_CONF_PATH, 'w') do |f|
-			f.write erb.result(binding)
+	def write_config_file content_path
+		puts "[Writing #{CONF_FILE_PATH} file for #{content_path}]"
+		File.open(CONF_FILE_PATH_TEMPLATE) do |f|
+			erb = ERB.new(f.read)
+			File.open(CONF_FILE_PATH, 'w') do |f|
+				f.write erb.result(binding)
+			end
 		end
 	end
-end
 
-def prep_cleanlist
-	CLEAN.include("content/**/*", "config/config_curr.rb")
-	CLEAN.exclude("content/bootstrap", "content/bootstrap/*", "content/bootstrap/**/*")
-end
+	def write_runtime_config
+		puts "[Writing #{RUN_CONF_PATH}]"
+		File.open(RUN_CONF_PATH_TEMPLATE) do |f|
+			erb = ERB.new(f.read)
+			File.open(RUN_CONF_PATH, 'w') do |f|
+				f.write erb.result(binding)
+			end
+		end
+	end
 
+	def smart_copy_files (files, source_dir, dest_dir)
+		files.each do |f|
+			dest_f = f.sub(source_dir, dest_dir)
+			mkdir_p dest_f.match(/(.*)\/.*/)[1]
+			cp_r f, dest_f
+		end
+	end
 
+	def prepare_cleanlist
+		CLEAN.include("content/**/*", "config/config_curr.rb")
+	end
 
-def blank? var
-	var.nil? || var.length == 0
-end
+	def content_master
+		CONTENT_PATH
+	end
 
-def config_ok?
-	!blank?(AWS_BUCKET) && !blank?(TOPICS_DIR)
-end
+	def topics_master
+		TOPICS_PATH
+	end
 
-def show_help
-	Rake::Task.tasks.each do |t|
-		puts "\n    #{t.name} - #{t.full_comment}"
+	def content_working
+		pwd+'/'
+	end
+
+	def topics_working
+		pwd+'/content/topics'
+	end
+
+	def blank? var
+		var.nil? || var.length == 0
+	end
+
+	def config_ok?
+		!blank?(AWS_BUCKET) && !blank?(TOPICS_PATH)
+	end
+
+	def show_help
+		Rake::Task.tasks.each do |t|
+			puts "\n    #{t.name} - #{t.full_comment}"
+		end
 	end
 end
